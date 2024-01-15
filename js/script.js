e738d42e05b26c60dca67f03eaf367e94f81ceb5
Sergii <sergiinari@gmail.com>
2024-01-15 23:05:36 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const inputUah = document.querySelector('#uah'),
+    inputUsd = document.querySelector('#usd');
+
+inputUah.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            console.log(request.response);
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputUah.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = "Щось пішло не так!!!";
+        }
+    });
+});
